74fb6e9086f08a64a7a92f17fc3e5d22bc379062
kkwafo <kkwafoawua@gmail.com>
2023-02-01 18:17:45 -0300

    change task

--- includes/header.php
+++ includes/header.php
@@ -19,30 +19,7 @@
             <a href="index.php" class= "navbar-brand " > To Do List (PHP) </a>
             
         </div> 
-       
-        <ul class="nav nav-pills">
-  <li class="nav-item">
-    <a class="btn btn-light create" href="createT.php?createT=" value="createT" > Create Table</a>
-  </li>
-  <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-    <div class="dropdown-menu">
-      <a class="dropdown-item" href="#">Action</a>
-      <a class="dropdown-item" href="#">Another action</a>
-      <a class="dropdown-item" href="#">Something else here</a>
-      <div class="dropdown-divider"></div>
-      <a class="dropdown-item" href="#">Separated link</a>
-    </div>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="#">Link</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled" href="#">Disabled</a>
-  </li>
-</ul>
-</div>
-
+        <a class="btn btn-light create" href="task.php?=" value="User"> Select Your Task </a>
         <a class="btn btn-light create" href="createT.php?createT=" value="createT" > Create Table</a>
      </nav>
 </head>
